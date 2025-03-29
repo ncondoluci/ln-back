72dd473568c3c006c9514a459d3cba1d89c22132
Nicolás Condoluci <condoluci.nico@gmail.com>
2025-03-29 11:45:33 -0300

    Agrega ruta /aacounts/getByFlag

--- src/routes/accountRoutes.ts
+++ src/routes/accountRoutes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { AccountController } from '../controllers/accountsController.js';
-import { tagChecker } from '../middlewares/tagChecker';
+import { flagChecker, tagChecker } from '../middlewares/tagChecker';
 
 const router = Router();
 
 const accountController = new AccountController();
 router.get('/getByTag', tagChecker, accountController.getAccountsByTag);
 
-router.get('/', [/* Middlewares */], (req, res, next) => res.json({success: true}));
+router.get('/getByFlag', flagChecker, accountController.getAccountsByFlag);
 
 export default router;
