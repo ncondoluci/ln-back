76932693d7dd2224779d3144d754fb2778092b4f
Nicolás Condoluci <condoluci.nico@gmail.com>
2025-03-29 11:43:23 -0300

    añade middleware para validar flag haveVoucher en query params

--- src/middlewares/tagChecker.ts
+++ src/middlewares/tagChecker.ts
@@ -1,11 +1,22 @@
 import { RequestHandler } from "express";
 
 export const tagChecker: RequestHandler = (req, res, next) => {
-    const {tag} = req.query; // Puede ser undefined, '' o 'cadena/string'
+    const { tag } = req.query; // Puede ser undefined, '' o 'cadena/string'
     if(typeof tag !== 'string' || tag.trim() === '') {
         res.status(400).json({
             success: false,
             message: 'Patámetro de busqueda "tag" no es correcto'
+        });
+        return;
+    }
+    next();
+}
+export const flagChecker: RequestHandler = (req, res, next) => {
+    const { haveVoucher } = req.query; // Puede ser undefined, '' o 'cadena/string'
+    if(typeof haveVoucher !== 'string' || haveVoucher.trim() === '') {
+        res.status(400).json({
+            success: false,
+            message: 'Patámetro de busqueda "haveVoucher" no es correcto'
         });
         return;
     }
