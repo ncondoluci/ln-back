7a2c0d14ae0e76fa8d4c5fdcb5728bfb995b0a37
Nicolás Condoluci <condoluci.nico@gmail.com>
2025-03-31 15:05:45 -0300

    Implementa AppError en controladores

--- src/controllers/accountsController.ts
+++ src/controllers/accountsController.ts
@@ -1,32 +1,73 @@
-import { RequestHandler } from 'express';
-import { createAccountService } from '../factories/accountServiceFactory';
-import { normalizeString } from '../utils/normalizeString';
+import { RequestHandler } from "express";
+import { createAccountService } from "../factories/accountServiceFactory";
+import { AppError } from "../utils/appError";
 
 const accountService = await createAccountService();
 
 export class AccountController {
-    getAccountsByTag: RequestHandler = async (req, res, next) => {
-        const { tag, offset = '0', limit = '4', orderAsc = 'true' } = req.query as Record<string, string>;
-        console.log(process.env);
+  getAccountsByTag: RequestHandler = (req, res, next) => {
+    const {
+      tag,
+      offset = "0",
+      limit = "4",
+      orderAsc = "true",
+    } = req.query as Record<string, string>;
 
-        try {
-            const accounts = await accountService.getAccountsVMByTag( tag, limit, offset, orderAsc );
-            res.status(200).json(accounts);
-        } catch (error) {
-            console.log(error);
-            next(error);    
-        }
+    try {
+      const accounts = accountService.getAccountsVMByTag(
+        tag,
+        limit,
+        offset,
+        orderAsc
+      );
+
+      res.status(200).json(accounts);
+    } catch (error: any) {
+      console.error(`Error en AccountController: ${error}`);
+
+      const appError =
+        error instanceof AppError
+          ? error
+          : new AppError({
+              message: error.message || "Error inesperado",
+              statusCode: 500,
+              isOperational: false,
+            });
+
+      next(appError);
     }
+  };
 
-    getAccountsByFlag: RequestHandler = async (req, res, next) => {
-        const { haveVoucher = 'true', offset = '0', limit = '4', orderDesc = 'true' } = req.query as Record<string, string>; 
-        
-        try {
-            const accounts = await accountService.getAccountByFlag( haveVoucher, limit, offset, orderDesc );
-            res.status(200).json(accounts);
-        } catch (error) {
-            console.log(error);
-            next(error);
-        }
-    } 
+  getAccountsByFlag: RequestHandler = (req, res, next) => {
+    const {
+      haveVoucher = "true",
+      offset = "0",
+      limit = "4",
+      orderDesc = "true",
+    } = req.query as Record<string, string>;
+
+    try {
+      const accounts = accountService.getAccountByFlag(
+        haveVoucher,
+        limit,
+        offset,
+        orderDesc
+      );
+
+      res.status(200).json(accounts);
+    } catch (error: any) {
+      console.error(`Error en AccountController: ${error}`);
+
+      const appError =
+        error instanceof AppError
+          ? error
+          : new AppError({
+              message: error.message || "Error inesperado",
+              statusCode: 500,
+              isOperational: false,
+            });
+
+      next(appError);
+    }
+  };
 }
